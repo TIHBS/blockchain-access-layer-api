fb87808193c62fb8696b90b5f0673c64dfcd75db
Akshay <akshay.ap95@gmail.com>
2022-03-10 22:28:33 +0100

    Change interface to register conneciton profile

--- src/main/java/blockchains/iaas/uni/stuttgart/de/api/IAdapterExtension.java
+++ src/main/java/blockchains/iaas/uni/stuttgart/de/api/IAdapterExtension.java
@@ -11,13 +11,17 @@
 
 package blockchains.iaas.uni.stuttgart.de.api;
 
+import blockchains.iaas.uni.stuttgart.de.api.connectionprofiles.AbstractConnectionProfile;
 import blockchains.iaas.uni.stuttgart.de.api.interfaces.BlockchainAdapter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.pf4j.ExtensionPoint;
 
 import java.util.Map;
 
 public interface IAdapterExtension extends ExtensionPoint {
-    BlockchainAdapter getAdapter(Map<String, Object> parameters);
+    BlockchainAdapter getAdapter(Class<? extends AbstractConnectionProfile> connectionProfile);
+
+    void registerConnectionProfile(ObjectMapper objectMapper);
 
     String getBlockChainId();
 }
