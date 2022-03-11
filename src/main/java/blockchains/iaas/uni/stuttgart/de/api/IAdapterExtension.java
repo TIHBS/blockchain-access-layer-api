da4359bdc3afe1a450f0dc148df2acb22db347cd
Akshay <akshay.ap95@gmail.com>
2022-03-11 11:20:37 +0100

    Update IAdapterExtension

--- src/main/java/blockchains/iaas/uni/stuttgart/de/api/IAdapterExtension.java
+++ src/main/java/blockchains/iaas/uni/stuttgart/de/api/IAdapterExtension.java
@@ -13,15 +13,14 @@ package blockchains.iaas.uni.stuttgart.de.api;
 
 import blockchains.iaas.uni.stuttgart.de.api.connectionprofiles.AbstractConnectionProfile;
 import blockchains.iaas.uni.stuttgart.de.api.interfaces.BlockchainAdapter;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.pf4j.ExtensionPoint;
-
-import java.util.Map;
 
 public interface IAdapterExtension extends ExtensionPoint {
     BlockchainAdapter getAdapter(AbstractConnectionProfile connectionProfile);
 
     Class<? extends AbstractConnectionProfile> getConnectionProfileClass();
+
+    String getConnectionProfileNamedType();
 
     String getBlockChainId();
 }
