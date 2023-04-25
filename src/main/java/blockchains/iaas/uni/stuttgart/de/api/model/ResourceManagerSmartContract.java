625dcdc2a8542c712d55d7b833461995d2c15c8d
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2023-04-25 17:28:59 +0200

    Update ResourceManagerSmartContract.java

--- src/main/java/blockchains/iaas/uni/stuttgart/de/api/model/ResourceManagerSmartContract.java
+++ src/main/java/blockchains/iaas/uni/stuttgart/de/api/model/ResourceManagerSmartContract.java
@@ -3,11 +3,17 @@ package blockchains.iaas.uni.stuttgart.de.api.model;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+import java.util.List;
+
+@NoArgsConstructor
 public abstract class ResourceManagerSmartContract extends SmartContract{
     public abstract SmartContractEvent getAbortEvent();
     public abstract SmartContractEvent getVoteEvent();
     public abstract SmartContractFunction getPrepareFunction();
     public abstract SmartContractFunction getAbortFunction();
     public abstract SmartContractFunction getCommitFunction();
+
+    public ResourceManagerSmartContract(String smartContractPath, List<SmartContractFunction> functions, List<SmartContractEvent> events) {
+        super(smartContractPath, functions, events);
+    }
 }
